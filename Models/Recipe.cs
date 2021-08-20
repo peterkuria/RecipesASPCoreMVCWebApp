cbe031ff30966e7d2b629b77c86b2f8358b9a25e
peterkuria <peter.kuria.cloud@gmail.com>
2021-08-20 13:01:34 +0300

    Add project files.

---
+++ Models/Recipe.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecipesASPCoreMVCWebApp.Models
+{
+    public class Recipe
+    {
+        // prop tab tab -> to create property
+        public int Id { get; set; }
+        public string RecipeName { get; set; }
+        public string RecipeCat { get; set; }
+        public DateTime RecipeDate { get; set; }
+        public string ingredients { get; set; }
+        public string PrepMtd { get; set; }
+        public int PrepTime { get; set; }
+        public int CookingTime { get; set; }
+        public int Servings { get; set; }
+
+        // ctor tab tab - to create  a function called constructor
+        public Recipe()
+        {
+
+        }
+
+    }
+
+    
+}
